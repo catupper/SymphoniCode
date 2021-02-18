b620057bad36bb2dba5e4c32b45cd94b9ee03c93
nerikosans <36780394+nerikosans@users.noreply.github.com>
2021-02-19 01:02:02 +0900

    Add blank background page

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,38 +1,39 @@
 // const webpack = require("webpack");
-const path = require("path");
-const CopyPlugin = require("copy-webpack-plugin");
-const srcDir = path.join(__dirname, "..", "src");
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const srcDir = path.join(__dirname, '..', 'src');
 
 module.exports = {
-    entry: {
-      symphony: path.join(srcDir, 'symphony.ts'),
+  entry: {
+    symphony: path.join(srcDir, 'symphony.ts'),
+    background: path.join(srcDir, 'background.ts'),
+  },
+  output: {
+    path: path.join(__dirname, '../dist/js'),
+    filename: '[name].js',
+  },
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: 'initial',
     },
-    output: {
-        path: path.join(__dirname, "../dist/js"),
-        filename: "[name].js",
-    },
-    optimization: {
-        splitChunks: {
-            name: "vendor",
-            chunks: "initial",
-        },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [{ from: ".", to: "../", context: "public" }],
-            options: {},
-        }),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: '.', to: '../', context: 'public' }],
+      options: {},
+    }),
+  ],
 };
