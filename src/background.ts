f356585aded36c94ba2cbaabc4d25e80aedc9cbc
nerikosans <36780394+nerikosans@users.noreply.github.com>
2021-02-19 01:22:25 +0900

    Add listener on atcoder test

---
+++ src/background.ts
@@ -0,0 +1,18 @@
+const atcoderRegEx = '^https://atcoder\\.jp/.*/custom_test.*$';
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Installed');
+
+  chrome.webNavigation.onCompleted.addListener(
+    (details) => {
+      console.log('loaded');
+      console.log(details.tabId, details.url);
+    },
+    {
+      url: [
+        {
+          urlMatches: atcoderRegEx,
+        },
+      ],
+    }
+  );
+});
