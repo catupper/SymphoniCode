f4e76b44048175260c9a35d030d19279a90c7ae9
nerikosans <36780394+nerikosans@users.noreply.github.com>
2021-02-21 16:18:09 +0900

    Move to atCoderDetector

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -5,9 +5,8 @@ const srcDir = path.join(__dirname, '..', 'src');
 
 module.exports = {
   entry: {
-    symphony: path.join(srcDir, 'symphony.ts'),
-    background: path.join(srcDir, 'background.ts'),
     codingPhase: path.join(srcDir, 'content_scripts/codingPhase.ts'),
+    atCoderDetector: path.join(srcDir, 'background/atCoderDetector.ts'),
     orchestra: path.join(srcDir, 'background/orchestra.ts'),
   },
   output: {
