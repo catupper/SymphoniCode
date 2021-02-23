2add8bf9036f572bdb29d04b5dbae028a8a0f434
Ken Ogura <nekarugo628@gmail.com>
2021-02-23 19:33:30 +0900

    implement parityUpDown

--- src/content_scripts/codingPhase.ts
+++ src/content_scripts/codingPhase.ts
@@ -9,12 +9,9 @@ const checkCodeLength = function (
 ) {
   const newLength = newCode.length;
   const oldLength = states['length'];
-  const lastStroke = states['strokes'];
   states['strokes'] += Math.abs(newLength - oldLength);
   states['length'] = newLength;
-  if (Math.floor(states['strokes'] / 5) - Math.floor(lastStroke / 5)) {
-    chrome.runtime.sendMessage({ length: length }, () => {});
-  }
+  chrome.runtime.sendMessage({ length: length }, () => {});
 };
 
 const initCodeLength = function (states: States) {
