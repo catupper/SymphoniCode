274e59033fddf0fd75ea2d2b1c6884a1af9e3bcc
Ken Ogura <nekarugo628@gmail.com>
2021-02-23 19:36:52 +0900

    add gentle comments

--- src/background/symphonicDj.ts
+++ src/background/symphonicDj.ts
@@ -10,9 +10,9 @@ export class SymphonicDj {
 
   /**
    * constructor
-   * @param {[]} samples: samples
-   * @param {function} transitionCallback: transitionCallback
-   * @param {string} initialState: initialState
+   * @param {{string: function}} samples: 状態(string)を開始するときに実行する関数
+   * @param {function} transitionCallback: 今の状態を引数として次の状態を返してくれる関数
+   * @param {string} initialState: 最初の状態
    */
   constructor(
     samples: { [state: string]: () => void },
@@ -29,9 +29,12 @@ export class SymphonicDj {
    */
   start() {
     const self = this;
+    // 4小節(4measures)ごとに一回 状態self.currentState に対応する関数を実行
     Tone.Transport.scheduleRepeat(() => {
       self.samples[self.currentState]();
     }, '4m');
+
+    // 2小節目から開始して4小節ごとに一回 状態self.currentStateを次の状態にアップデート
     Tone.Transport.scheduleRepeat(
       () => {
         self.currentState = self.transitionCallback(self.currentState);
